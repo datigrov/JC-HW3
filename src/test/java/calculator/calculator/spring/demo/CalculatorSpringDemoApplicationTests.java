03103b911c510c216645c04b4d50d078c456bcbd
datigrov <denistigrov@icloud.com>
2023-12-24 19:57:45 +0300

    add git

---
+++ src/test/java/calculator/calculator/spring/demo/CalculatorSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package calculator.calculator.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
