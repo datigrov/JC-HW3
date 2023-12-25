0ebf3d50e7e030bf5ceb5b2852246a0d680d6bcd
datigrov <denistigrov@icloud.com>
2023-12-25 22:40:05 +0300

    complete

--- src/main/java/calculator/calculator/spring/demo/CalculatorController.java
+++ src/main/java/calculator/calculator/spring/demo/CalculatorController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
     private CalculatorService calculatorService;
+
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
@@ -14,28 +15,29 @@ public class CalculatorController {
     public String hello() {
         return "Добро пожаловать в калькулятор";
     }
+
     @GetMapping("/calculator/plus")
-    public int plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
-        return num1 + num2;
+    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        int result = Integer.valueOf(num1) + Integer.valueOf(num2);
+        return num1 + " + " + num2 + " = " + result;
     }
 
     @GetMapping("/calculator/minus")
-    public int minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
-        return num1 - num2;
+    public String minus(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
+        return num1 + " - " + num2 + " = " + calculatorService.minus(num1, num2);
     }
 
     @GetMapping("/calculator/multiply")
-    public int multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
-        return num1 * num2;
+    public String multiply(@RequestParam(required = true) int num1, @RequestParam(required = true) int num2) {
+        return num1 + " * " + num2 + " = " + calculatorService.multiply(num1, num2);
     }
 
-    @GetMapping("/calculator/divide")
-    public int divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+    @GetMapping("/calculator/devide")
+    public String divide(@RequestParam(required = true) double num1, @RequestParam(required = true) double num2) {
         if (num2 == 0) {
             System.out.println("Делить на ноль нельзя");
         }
-        return num1 / num2;
+        return num1 + " / " + num2 + " = " + calculatorService.devide(num1, num2);
+
     }
-
-
 }
